bf4f96456e5d76640a0f57f74af19a92681d478e
Redakas <62571380+andregabrielmelo@users.noreply.github.com>
2026-08-03 13:27:28 -0300

    fix: make a private constructor for address value object

--- backend/src/KinGraph.Core/ValueObjects/Address.cs
+++ backend/src/KinGraph.Core/ValueObjects/Address.cs
@@ -8,6 +8,8 @@ public class Address : ValueObject
     public string PostalCode { get; private set; }
     public Country Country { get; private set; }
 
+    private Address() { } // TODO: Made to work wirh EF core in entity configurations, consider a better way to handle this
+
     public Address(string street, City city, State state, string postalCode, Country country)
     {
         Street = street;
